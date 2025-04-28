ed480bcc6b21fe7ce67c103f09687e5cc42316d1
Giridharansanthoshkumar <giridharansanthosh0501@gmail.com>
2025-04-28 22:54:18 +0530

    added the script for downloading model

--- backend/Download_model.sh
+++ backend/Download_model.sh
@@ -1,4 +1,4 @@
 
 # Step 2: Download your model from Google Drive
 echo "Downloading model from Google Drive..."
-gdown --folder "https://drive.google.com/drive/folders/YOUR_FOLDER_ID_HERE"
+gdown --folder "https://drive.google.com/drive/folders/1t5KG9hk1JUfhTVCfrrA1d2OAUAedAgeL"
