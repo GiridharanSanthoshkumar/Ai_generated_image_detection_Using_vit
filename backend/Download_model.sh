433f17759fd3c6a8ed9a8c10c44d24e0c3987ab6
Giridharansanthoshkumar <giridharansanthosh0501@gmail.com>
2025-04-29 12:47:29 +0530

    readme file updated

--- backend/Download_model.sh
+++ backend/Download_model.sh
@@ -1,4 +1,13 @@
 
-# Step 2: Download your model from Google Drive
-echo "Downloading model from Google Drive..."
-gdown --folder "https://drive.google.com/drive/folders/1t5KG9hk1JUfhTVCfrrA1d2OAUAedAgeL"
+# Check if gdown is installed, if not, install it
+if ! command -v gdown &> /dev/null
+then
+    echo "gdown not found. Installing gdown..."
+    python3 -m pip install gdown
+fi
+
+# Download model from Google Drive
+# Replace YOUR_FILE_ID with the actual file ID from your Google Drive link
+python3 -m gdown --folder "https://drive.google.com/drive/folders/1t5KG9hk1JUfhTVCfrrA1d2OAUAedAgeL?usp=sharing"
+
+
